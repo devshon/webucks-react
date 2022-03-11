8435e4305b91cf3cecf43dbeae32c104c27e20b9
shon97 <wjdgns200297@gmail.com>
2022-03-11 12:08:07 +0900

    Add: first setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import "../src/styles/Reset.scss";
+import "../src/styles/Common.scss";
+
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Detail from "./pages/detail/Detail";
+import Signup from "./pages/signup/Signup";
+
+function Router() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default Router;
---
+++ src/pages/login/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import "./Login.scss";
+
+const regId = /^([0-9a-zA-Z_.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+const regPw = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+function _Login() {
+  const navigate = useNavigate();
+  const [idText, setIdText] = useState("");
+  const [pwText, setPwText] = useState("");
+  const [isShowPw, setIsShowPw] = useState(false);
+  const isActiveId = regId.test(idText);
+  const isActivePw = regPw.test(pwText);
+  const isAbleLogin = isActiveId && isActivePw;
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter" && isAbleLogin) {
+      handleLoginBtn();
+    }
+  }
+
+  function handleLoginBtn() {
+    navigate("/list", { state: { username: idText } });
+    // const params = JSON.stringify({
+    //   email: idText,
+    //   password: pwText,
+    // });
+    // const headers = {
+    //   'Content-Type': 'application/json',
+    //   Accept: '*/*',
+    // };
+    // axios.post('/users/login', params, { headers }).then(response => {
+    //   if (response.statusText === 'OK') {
+    //     localStorage.setItem('webucks_token', response.data.token);
+    //     navigate('/list', { state: { username: idText } });
+    //   } else {
+    //     alert('아이디 비밀번호를 재확인 해주세요');
+    //   }
+    // });
+  }
+  return (
+    <section className="login_main">
+      <article>
+        <header>
+          <img alt="webucks" src="images/icon_webucks.png" />
+        </header>
+        <div className="input_container">
+          <input
+            id="login_id"
+            className={isActiveId ? "active_input" : ""}
+            type="text"
+            maxLength="20"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onKeyPress={handleKeyPress}
+            onChange={(e) => setIdText(e.target.value)}
+          />
+          <div className="login_password_wrap">
+            <input
+              id="login_pw"
+              className={isActivePw ? "active_input" : ""}
+              type={isShowPw ? "text" : "password"}
+              maxLength="20"
+              placeholder="비밀번호"
+              onKeyPress={handleKeyPress}
+              onChange={(e) => setPwText(e.target.value)}
+            />
+            <span onClick={() => setIsShowPw(!isShowPw)} id="show_button">
+              {isShowPw ? "hide" : "show"}
+            </span>
+          </div>
+          <button
+            type="button"
+            className={isAbleLogin ? "active_btn" : ""}
+            onClick={handleLoginBtn}
+            disabled={!isAbleLogin}
+          >
+            로그인
+          </button>
+        </div>
+        <footer>
+          <Link to="/signup">회원가입 하러가기</Link>
+        </footer>
+      </article>
+    </section>
+  );
+}
+
+export default _Login;
